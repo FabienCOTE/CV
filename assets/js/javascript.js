43e841d67c98d279035d49682c580d557127aecc
FabienCOTE <fabien.cote@gmail.com>
2018-09-25 11:26:31 +0200

    Ajout des fonctions javascript scroll animated

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,31 @@
+window.onscroll = function() {myFunction()};
+
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      document.getElementById("mooveSlide1").className = "slideInLeft animated";
+      document.getElementById("mooveSlide1").style.display = "block";
+      } else {
+      document.getElementById("mooveSlide1").style.display = "none";
+      }
+
+    if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+      document.getElementById("mooveSlide2").className = "slideInRight animated";
+      document.getElementById("mooveSlide2").style.display = "block";
+      } else {
+      document.getElementById("mooveSlide2").style.display = "none";
+      }
+
+    if (document.body.scrollTop > 2700 || document.documentElement.scrollTop > 2700) {
+      document.getElementById("mooveSlide3").className = "slideInLeft animated";
+      document.getElementById("mooveSlide3").style.display = "block";
+      } else {
+      document.getElementById("mooveSlide3").style.display = "none";
+      }
+
+    if (document.body.scrollTop > 3900 || document.documentElement.scrollTop > 3900) {
+      document.getElementById("mooveSlide4").className = "slideInRight animated";
+      document.getElementById("mooveSlide4").style.display = "block";
+      } else {
+      document.getElementById("mooveSlide4").style.display = "none";
+      }
     }
-}
