36ca8c9a99f3dedfe31788b9fbef3af36491a20c
FabienCOTE <fabien.cote@gmail.com>
2018-09-25 11:49:54 +0200

    Ajout du bouton scroll top

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,31 +1,42 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-      document.getElementById("mooveSlide1").className = "slideInLeft animated";
-      document.getElementById("mooveSlide1").style.display = "block";
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("mooveSlide1").className = "slideInLeft animated";
+        document.getElementById("mooveSlide1").style.display = "block";
       } else {
-      document.getElementById("mooveSlide1").style.display = "none";
+        document.getElementById("mooveSlide1").style.display = "none";
       }
 
-    if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
-      document.getElementById("mooveSlide2").className = "slideInRight animated";
-      document.getElementById("mooveSlide2").style.display = "block";
+    if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400) {
+        document.getElementById("mooveSlide2").className = "slideInRight animated";
+        document.getElementById("mooveSlide2").style.display = "block";
       } else {
-      document.getElementById("mooveSlide2").style.display = "none";
+        document.getElementById("mooveSlide2").style.display = "none";
       }
 
-    if (document.body.scrollTop > 2700 || document.documentElement.scrollTop > 2700) {
-      document.getElementById("mooveSlide3").className = "slideInLeft animated";
-      document.getElementById("mooveSlide3").style.display = "block";
+    if (document.body.scrollTop > 2500 || document.documentElement.scrollTop > 2500) {
+        document.getElementById("mooveSlide3").className = "slideInLeft animated";
+        document.getElementById("mooveSlide3").style.display = "block";
       } else {
-      document.getElementById("mooveSlide3").style.display = "none";
+        document.getElementById("mooveSlide3").style.display = "none";
       }
 
-    if (document.body.scrollTop > 3900 || document.documentElement.scrollTop > 3900) {
-      document.getElementById("mooveSlide4").className = "slideInRight animated";
-      document.getElementById("mooveSlide4").style.display = "block";
+    if (document.body.scrollTop > 3700 || document.documentElement.scrollTop > 3700) {
+        document.getElementById("mooveSlide4").className = "slideInRight animated";
+        document.getElementById("mooveSlide4").style.display = "block";
       } else {
-      document.getElementById("mooveSlide4").style.display = "none";
+        document.getElementById("mooveSlide4").style.display = "none";
+      }
+
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("myBtn").style.display = "block";
+      } else {
+        document.getElementById("myBtn").style.display = "none";
       }
     }
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
