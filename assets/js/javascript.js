0bf04aeddf041390c944ab0c63f8557abd37ef0b
FabienCOTE <fabien.cote@gmail.com>
2018-09-26 11:18:37 +0200

    Modification du header

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("mooveSlide1").className = "slideInLeft animated";
         document.getElementById("mooveSlide1").style.display = "block";
       } else {
